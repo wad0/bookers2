262cc5441d998a3612ec8e06956b6f8a18e90f4f
wad0 <fukushima0102@gmail.com>
2021-04-03 13:30:02 +0000

    0403_1

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :book_comments,dependent: :destroy
 
   validates :title, presence: true
   validates :body, length: { minimum: 1, maximum: 200 }
