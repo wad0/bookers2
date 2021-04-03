d5a850fc024122726529fc690f5b0da3a219ccfa
wad0 <fukushima0102@gmail.com>
2021-04-03 15:03:26 +0000

    0404_1

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'homes#top'
   get'home/about' => 'homes#about'
-  resources :books
+  resources :books do
+    resources :book_comments,only:[:create,:destroy]
+  end
   resources :users,only:[:create,:index,:show,:edit,:update]
 end
