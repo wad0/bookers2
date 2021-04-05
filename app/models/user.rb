c2285a9b991a3b60fb385e5796156145d680a779
wad0 <fukushima0102@gmail.com>
2021-04-05 06:04:30 +0000

    0405_6

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,20 @@ class User < ApplicationRecord
 
   validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
+  
+  # ユーザーをフォローする
+def follow(user_id)
+  follower.create(followed_id: user_id)
+end
+
+# ユーザーのフォローを外す
+def unfollow(user_id)
+  follower.find_by(followed_id: user_id).destroy
+end
+
+# フォローしていればtrueを返す
+def following?(user)
+  following_user.include?(user)
+end
+  
 end
