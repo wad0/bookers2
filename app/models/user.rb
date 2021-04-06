9be1ed6cc65daacdeda813db9e49988289d9763e
wad0 <fukushima0102@gmail.com>
2021-04-06 10:57:12 +0000

    0406_1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,32 +6,35 @@ class User < ApplicationRecord
   has_many :books,dependent: :destroy
   has_many :book_comments,dependent: :destroy
   has_many :favorites,dependent: :destroy
-  has_many :follower,class_name: "Relationship",foreign_key: "follower_id",dependent: :destroy
-  has_many :followed,class_name: "Relationship",foreign_key: "followed_id",dependent: :destroy
-  # foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定
-  
-  has_many :following_user, through: :follower, source: :followed 
-  has_many :follower_user, through: :followed, source: :follower 
-  # @user.booksのように、@user.○○で、そのユーザがフォローしている人orフォローされている人の一覧を出したい
-  
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+
+  # has_many :follower,class_name: "Relationship",foreign_key: "follower_id",dependent: :destroy
+  # has_many :followed,class_name: "Relationship",foreign_key: "followed_id",dependent: :destroy
+  # # foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定
+
+  # has_many :following_user, through: :follower, source: :followed
+  # has_many :follower_user, through: :followed, source: :follower
+  # # @user.booksのように、@user.○○で、そのユーザがフォローしている人orフォローされている人の一覧を出したい
+
   attachment :profile_image
 
   validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
-  
-  # ユーザーをフォローする
-def follow(user_id)
-  follower.create(followed_id: user_id)
-end
 
-# ユーザーのフォローを外す
-def unfollow(user_id)
-  follower.find_by(followed_id: user_id).destroy
-end
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
 
-# フォローしていればtrueを返す
-def following?(user)
-  following_user.include?(user)
-end
-  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
