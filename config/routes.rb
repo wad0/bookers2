8b69add6ccbc42bbf64fdba3b00afa2a7025f863
wad0 <fukushima0102@gmail.com>
2021-04-07 13:54:14 +0000

    0407_1_search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'search/search'
   devise_for :users
   root to:'homes#top'
   get'home/about' => 'homes#about'
+  get'/search' => 'search#sea'
+  
   resources :books do
     resource :favorites,only:[:create,:destroy]
     resources :book_comments,only:[:create,:destroy]
