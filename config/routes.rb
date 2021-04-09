92c74a9de584803460c0078f37c61e99632d40b0
wad0 <fukushima0102@gmail.com>
2021-04-09 14:54:41 +0000

    0409_2

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :book_comments,only:[:create,:destroy]
   end
   resources :users,only:[:create,:index,:show,:edit,:update] do
-    resource :relationships, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
